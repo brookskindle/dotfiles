6023c5832a44a806ae2a6b8ac6b839bb8f0e62ef
Brooks Kindle <Brooks.Kindle@emc.com>
2015-10-21 11:12:37 -0700

    Edited lockscreen script.

--- bin/bin/lock.sh
+++ bin/bin/lock.sh
@@ -3,6 +3,5 @@
 tmpimg='/tmp/lockscreen.png'
 
 scrot -zq 100 "$tmpimg"
-convert -resize 10% $tmpimg $tmpimg
-convert -resize 1000% $tmpimg $tmpimg
+convert -scale 10% -scale 1000% $tmpimg $tmpimg
 i3lock --image=$tmpimg
