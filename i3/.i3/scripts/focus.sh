79a3fceacb6acf2283e9fe9408a3bf508a1904fc
Brooks Kindle <brookskindle+git@gmail.com>
2018-05-27 00:22:14 -0700

    Replace rofi --> trofi
    A script of my own making, a wrapper around rofi to make windows
    semi-transparent before calling rofi, and then restoring afterwards.
    
    Find it at <dotfiles>/rofi/bin/trofi

--- i3/.i3/scripts/focus.sh
+++ i3/.i3/scripts/focus.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # Opens dmenu up to select the workspace you want to jump to
-DEST=$(i3-msg -t get_workspaces | jq '.[] | .name' -r | rofi -dmenu -i -p "Select your workspace:")
+DEST=$(i3-msg -t get_workspaces | jq '.[] | .name' -r | trofi -dmenu -i -p "Select your workspace:")
 i3-msg workspace $DEST
