3dc59599cd501a51665a8eb1dfd92a1aaa82054b
Brooks Kindle <brookskindle+git@gmail.com>
2020-07-08 20:43:21 -0700

    [i3] focus.sh: don't suggest new workspace names
    It was a neat idea, to pre-populate the first rofi choice with a random
    word, but I ended up not using it ever. I like naming my workspaces
    manually way too much.

--- i3/.i3/scripts/focus.sh
+++ i3/.i3/scripts/focus.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
-CURRENT=$(i3-msg -t get_workspaces | jq -r '.[].name')
-
-NEW="$(shuf -n1 /usr/share/dict/cracklib-small)"
-
-WORKSPACES=$(echo "$CURRENT" | awk -v new="$NEW" 'NR==1{print new}{print $1}')
-
-CHOSEN=$(echo "$WORKSPACES" | rofi -dmenu -i -p "Select your workspace:")
-i3-msg workspace "$CHOSEN"
+WORKSPACE=$(i3-msg -t get_workspaces | jq -r '.[].name' | rofi -dmenu -i -p "Select your workspace:")
+i3-msg workspace "$WORKSPACE"
