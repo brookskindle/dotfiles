d0422eb09f9625b3fa6ee4cce253d12f7ee08138
Brooks Kindle <brookskindle+git@gmail.com>
2020-07-08 20:36:59 -0700

    Replace xfce4-display-settings with layout script

---
+++ arandr/.local/bin/select-monitor-layout.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# Simple script that uses rofi and arandr-generated scripts to switch between
+# pre-configured monitor layouts.
+
+cd ~/.screenlayout
+ls | rofi -dmenu -p "Set monitor layout" | xargs sh
+
+# Re-apply wallpaper background
+nitrogen --restore
