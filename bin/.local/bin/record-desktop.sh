07fbffff8710ebb3df3204c86f0f63f545195299
Brooks Kindle <brookskindle+git@gmail.com>
2020-07-24 18:31:18 -0700

    Grab primary screen when recording desktop

--- bin/.local/bin/record-desktop.sh
+++ bin/.local/bin/record-desktop.sh
@@ -7,10 +7,13 @@ if [ "$1" == "start" ]; then
     cd ~/Videos
     # Start webcam
     # ffplay -f video4linux2 -i /dev/video0 -video_size 320x240 &
+
+    # Record screen
     OUTPUT_FILE="$(date +"%c" | tr A-Z' ' a-z'_').mkv"
     ffmpeg \
         -f alsa -i hw:0 \
-        -f x11grab -r 25 -s 1920x1080 -i :0.0+1920 -vcodec libx264 $OUTPUT_FILE
+        -f x11grab -r 25 -s 1920x1080 -i :0.0 -vcodec libx264 $OUTPUT_FILE
+        # -f x11grab -r 25 -s 1920x1080 -i :0.0+1920 -vcodec libx264 $OUTPUT_FILE
 elif [ "$1" == "stop" ]; then
     pkill ffmpeg
 else
