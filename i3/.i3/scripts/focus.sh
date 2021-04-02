0cc954ddbe3420a88d380df039de631ab32db33e
Brooks Kindle <brookskindle+git@gmail.com>
2021-04-01 22:54:21 -0700

    Remove additional colon ':' in rofi prompt scripts

--- i3/.i3/scripts/focus.sh
+++ i3/.i3/scripts/focus.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-WORKSPACE=$(i3-msg -t get_workspaces | jq -r '.[].name' | rofi -dmenu -i -p "Select your workspace:")
+WORKSPACE=$(i3-msg -t get_workspaces | jq -r '.[].name' | rofi -dmenu -i -p "Select your workspace")
 i3-msg workspace "$WORKSPACE"
