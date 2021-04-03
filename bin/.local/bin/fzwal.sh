5a09cc0d8a0c6284af70123623f5287a846098e5
Brooks Kindle <brookskindle+git@gmail.com>
2021-04-03 15:18:14 -0700

    [pywal] Simplify fzwal.sh to only show user themes

--- bin/.local/bin/fzwal.sh
+++ bin/.local/bin/fzwal.sh
@@ -1,40 +1,16 @@
 #!/bin/sh
-# Lists dark themes by default.
-# Light themes are listed if an argument is passed.
-# Export FZWAL_RESET_CURSOR=1 if you don't want Pywal to change
-# the default behaviour of the terminal cursor (inverse fg and bg).
-#
-# Script taken from: https://github.com/Kharacternyk/fzwal
+# A simplified version of fzwal.sh that only lists user themes
+#   https://github.com/Kharacternyk/fzwal
 
 cp -f ~/.cache/wal/colors.json /tmp/fzwal-backup.json
 
-if [ -n "$1" ]; then
-    IS_LIGHT=TRUE
-    THEME=$(wal --theme |
-            sed '1,/Light Themes/d;/Extra/,$d' |
-            sed -e '/^\S/d' -e 's/ - //' |
-            fzf --preview='wal -qetl --theme {} && wal --preview')
-else
-    THEME=$(wal --theme |
-            sed '/Light Themes/,$d' |
-            sed -e '/^\S/d' -e 's/ - //' |
-            fzf --preview='wal -qet --theme {} && wal --preview')
+THEME=$(ls ~/.config/wal/colorschemes/dark |
+        cut -d '.' -f 1 |
+        fzf --preview='wal -qet --theme {} && wal --preview')
+
+# No theme chosen
+if [ -z "$THEME" ]; then
+    THEME="/tmp/fzwal-backup.json"
 fi
 
-
-if [ -n "$THEME" ]; then
-    if [ -n "$IS_LIGHT" ]; then
-        wal -ql --theme $THEME
-    else
-        wal -q --theme $THEME
-    fi
-else
-    wal -q --theme /tmp/fzwal-backup.json
-fi
-
-if [ -n "$FZWAL_RESET_CURSOR" ]; then
-    for TTY in /dev/pts/*; do
-        [ -w $TTY ] && /bin/printf "\e]112\a" > $TTY
-    done
-    exit 0
-fi
+wal -q --theme $THEME
